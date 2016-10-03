63e581b08c9a0e7667c24a94ae1e24ae17fef76d
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-03 08:42:24 -0700

    add hashHistory

--- config/routes.js
+++ config/routes.js
@@ -4,11 +4,13 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+// hashHistory, browserHistory, memoryHistory
+var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
 
 var routes = (
-  <Router>
+  <Router history={hashHistory}>
     <Route path='/' component={Main}>
       {/* Home is active when none of the other routes match  */}
       <IndexRoute component={Home} />
