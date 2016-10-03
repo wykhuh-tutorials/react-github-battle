372ba66994091f0ab7d26a93a8280011c90d0d15
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-03 08:40:43 -0700

    add IndexRoute from Home

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,17 @@
+
 var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var IndexRouter = ReactRouter.IndexRoute;
-
+var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
-var Home = require('../components/Home');
+var Home = require("../components/Home");
 
 var routes = (
   <Router>
     <Route path='/' component={Main}>
-      <Route path='/home' component={Home}></Route>
+      {/* Home is active when none of the other routes match  */}
+      <IndexRoute component={Home} />
     </Route>
   </Router>
 );
