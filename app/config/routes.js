f5656064d9849a628db8e0feda2812266f62d019
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-04 07:20:54 -0700

    display ConfirmBattleContainer

--- app/config/routes.js
+++ app/config/routes.js
@@ -9,12 +9,15 @@ var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
 var PromptContainer = require("../containers/PromptContainer");
+var ConfirmBattleContainer = require("../containers/ConfirmBattleContainer");
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <Route path='playerOne' header='Player One' component={PromptContainer}/>
       <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer}/>
+      <Route path='battle' component={ConfirmBattleContainer}/>
+
       {/* Home is active when none of the other routes match  */}
       <IndexRoute component={Home} />
     </Route>
