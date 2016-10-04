de2063067f8e3e399b3c40fd2f0354fddb3ce4e9
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-03 20:26:47 -0700

    add PromptContainer for playerOne and playerTwo

--- app/config/routes.js
+++ app/config/routes.js
@@ -8,10 +8,13 @@ var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
+var PromptContainer = require("../containers/PromptContainer");
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
+      <Route path='playerOne' header='Player One' component={PromptContainer}/>
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer}/>
       {/* Home is active when none of the other routes match  */}
       <IndexRoute component={Home} />
     </Route>
