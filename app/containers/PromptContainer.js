df810b0878cbcc10853e5be876eae3c232b2097d
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-03 21:09:49 -0700

    create separte Prompt component for rendering

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
   // context allow you to pass items without going to props;
@@ -18,13 +18,13 @@ var PromptContainer = React.createClass({
   },
 
   // when input value changes, update this.state.username
-  onUpdateUser: function(e) {
+  handleUpdateUser: function(e) {
     this.setState({
       username: e.target.value
     })
   },
 
-  onSubmitUser: function(e) {
+  handleSubmitUser: function(e) {
     e.preventDefault();
     // cache this.state.username so user doesn't get old this.state.username
     // if they hit the back button
@@ -58,28 +58,11 @@ var PromptContainer = React.createClass({
 
   render: function() {
     return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-        <h1>{this.props.route.header}</h1>
-        <div className="col-sm-12">
-          <form onSubmit={this.onSubmitUser}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                onChange={this.onUpdateUser}
-                value={this.state.username}
-                placeholder="Github Username"/>
-            </div>
-            <div className="form-group col-sm-3 col-sm-offset-4">
-              <button
-                className="btn btn-block btn-success"
-                type="submit">
-                Continue
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt
+        onSubmitUser={this.handleSubmitUser}
+        onUpdateUser={this.handleUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username} />
     )
   }
 })
