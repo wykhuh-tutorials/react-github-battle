1551bbe60772faf9ab5e3e1f0e1095b37540c191
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-04 07:20:54 -0700

    display ConfirmBattleContainer

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,25 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+  // use contextTypes to get access to the router
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  getInitialState: function() {
+    return {
+      isLoading: true,
+      playerInfo: []
+   }
+  },
+
+  render: function() {
+    return (
+      <ConfirmBattle />
+    );
+  }
+
+});
+
+module.exports = ConfirmBattleContainer;
