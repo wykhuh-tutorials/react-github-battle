34cd0c4f1d7c0432c62f80a303563d992f326ce2
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-04 23:10:14 -0700

    githubService to fetch data from github

---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,29 @@
+var axios = require('axios');
+var githubConfig = require('../config/githubConfig');
+
+var clientId = githubConfig.clientId;
+var clientSecret = githubConfig.clientSecret;
+var param = '?client_id=' + clientId + "&client_secret" + clientSecret;
+
+function getUserInfo(username) {
+  return axios.get('https://api.github.com/users/' + username + param)
+}
+
+var helpers = {
+  getPlayersInfo: function(players) {
+    // axios.all takens an array of promises;
+    // when all promises are resovled, then do then()
+    return axios.all(players.map(function (username) {
+      return getUserInfo(username);
+    })).then(function(info) {
+      // only want the data property from the user github sends
+      return info.map(function(user) {
+        return user.data;
+      })
+    }).catch(function(err) {
+      console.warn('Erron in getPlayersInfo.', err)
+    })
+  }
+}
+
+module.exports = helpers;
