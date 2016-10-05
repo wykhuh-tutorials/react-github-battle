cec699dffaf142445ee0663222266bd4bc4b3863
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-05 04:15:24 -0700

    use output to display son for playersInfo on ConfirmBattle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -2,6 +2,10 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
 
+function output (object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
 function ConfirmBattle (props) {
     return props.isLoading === true
       ? <p>Loading...</p>
@@ -10,11 +14,11 @@ function ConfirmBattle (props) {
         <div className='col-sm-8 col-sm-offset-2'>
           <div className="col-sm-6">
             <p className="lead">Player 1</p>
-            Player one info
+            {output(props.playersInfo[0])}
           </div>
           <div className="col-sm-6">
             <p className="lead">Player 2</p>
-            Player two info
+            {output(props.playersInfo[1])}
           </div>
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
