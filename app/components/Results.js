17cdefb1409babc218f2a0140e9c8d1fef2c06b6
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-06 21:35:35 -0700

    finish GUI for Results

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,14 +1,28 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-
-function output (object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
+var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 
 function Results (props) {
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
+
   return (
-    <div>{output(props)}</div>
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparent}>
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]}/>
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header="Loser">
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]}/>
+
+        </UserDetailsWrapper>
+      </div>
+
+    </div>
   );
 }
 
