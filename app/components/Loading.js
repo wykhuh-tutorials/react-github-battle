46532476c651047bd01d5a159eb067c1b571c569
Wai-Yin Kwan <wykhuh@icloud.com>
2016-10-07 04:08:48 -0700

    add Loading component

---
+++ app/components/Loading.js
@@ -0,0 +1,59 @@
+var React = require('react');
+
+var styles = {
+  container: {
+    position: 'fixed',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    fontSize: '55px'
+  },
+  content: {
+    textAlign: 'center',
+    position: 'absolute',
+    width: '100%',
+    marginTop: '30px',
+  }
+}
+
+var Loading = React.createClass({
+  originalText: 'Loading',
+
+  getInitialState: function(){
+    return {
+      text: this.originalText
+    }
+  },
+
+  componentDidMount: function(){
+    var stopper = this.originalText + "...";
+    this.interval = setInterval(function(){
+      // if not 'Loading...', add '.' at every interval
+      if(this.state.text === stopper) {
+        this.setState({
+          text: this.originalText
+        })
+      } else {
+        this.setState({
+          text:  this.state.text + '.'
+        })
+      }
+    }.bind(this), 300)
+  },
+
+  componentWillUnmount: function () {
+    // stop componentDidMount setInterval
+    clearInterval(this.interval);
+  },
+
+  render: function() {
+    return (
+      <div style={styles.container}>
+        <p style={styles.content}>{this.state.text}</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Loading;
